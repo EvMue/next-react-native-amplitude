27513218f6a273af89882c48c47f6e978824da71
Evelyn Mueller <evelyn.mueller@horizon-alpha.com>
2022-02-08 16:46:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Amplitude JS (for Next.js) + Amplitude React Native 🥳
+
+A set of hooks that wrap the `react-navigation` API that you're used to, and make it work with `next/router`.
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { useNavigation, useLinkTo } from './hooks/use-Navigation'
+import * as NavigationTypes from './hooks/use-navigation/types'
+export { NavigationTypes, useNavigation, useLinkTo }
