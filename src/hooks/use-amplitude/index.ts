fb95886b2060ac9d7d1ebefbec5347579c37dc0c
Evelyn Mueller <evelyn.mueller@horizon-alpha.com>
2022-02-09 13:21:01 +0100

    added log for native

--- src/hooks/use-amplitude/index.ts
+++ src/hooks/use-amplitude/index.ts
@@ -1,6 +1,7 @@
 import { Amplitude } from '@amplitude/react-native'
 
 export function getInstance() {
+  console.log("native amplitude version")
   const ampInstance = Amplitude.getInstance()
   return ampInstance
 }
