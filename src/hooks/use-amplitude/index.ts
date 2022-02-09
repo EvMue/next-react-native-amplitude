c31595438c856a3f03967d14b2930158b0ef8a66
Evelyn Mueller <evelyn.mueller@horizon-alpha.com>
2022-02-09 11:35:55 +0100

    setLibrary

--- src/hooks/use-amplitude/index.ts
+++ src/hooks/use-amplitude/index.ts
@@ -1,1 +1,8 @@
-export { getInstance} from 'amplitude-js';
+import { getInstance, setLibrary } from 'amplitude-js'
+// here
+
+export function getInstance() {
+  const ampInstance = getInstance()
+  setLibrary('next-react-native-amplitude', '1.0.0')
+  return ampInstance
+}
