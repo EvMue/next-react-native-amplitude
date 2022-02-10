f501d1a8be74e318b81bc794f2c87252e5536176
Evelyn Mueller <evelyn.mueller@horizon-alpha.com>
2022-02-10 12:10:16 +0100

    reverted index change

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-// import { getInstance} from './hooks/use-amplitude';
+import { getInstance} from './hooks/use-amplitude';
+// // import * as NavigationTypes from './hooks/use-amplitude/types'
+export { getInstance }
 
-// console.log("index");
-// // // import * as NavigationTypes from './hooks/use-amplitude/types'
-// export { getInstance }
-
-const useAmplitude = require("./hooks/use-amplitude");
-export default useAmplitude;
+// const useAmplitude = require("./hooks/use-amplitude");
+// export default useAmplitude;
